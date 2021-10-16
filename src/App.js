ea449a201d25c31bbec6fbc0cbe68f44fc331389
Bellik888 <Bellik888@gmail.com>
2021-10-16 21:51:43 +0300

    add locale storage

--- src/App.js
+++ src/App.js
@@ -8,13 +8,26 @@ import { ContactListItem } from './components/ContactListItem/ContactListItem';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    let localeContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (localeContacts) {
+      this.setState({ contacts: localeContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addNewContact = obj => {
     this.setState(prevState => {
